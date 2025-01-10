e792337f32a5a61deebd7910737391ca7ad5ae64
Mubeen K. <mubeenk.exe@gmail.com>
2025-01-10 19:24:52 +0500

    Update greet.js: Modify welcome message to include a penguin emoji and enhance the greeting text for a more engaging user experience.

--- templates/express/javascript/base/src/utils/greet.js
+++ templates/express/javascript/base/src/utils/greet.js
@@ -1,5 +1,5 @@
 export default function greet(req, res) {
     res.send(
-        '<span style="display: flex; justify-content: center; justify-items: center; font-size: 24px;"> <p>Welcome To Node.js application built with Express.js</p></span>'
+        '<span style="display: flex; justify-content: center; justify-items: center; font-size: 24px;"> <p>🐧 Welcome to the juicy Node.js application built with Express.js</p></span>'
     );
 }
